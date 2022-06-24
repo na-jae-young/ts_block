b4d3af95fac62e05dc4f64466091b19dc9bfd328
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-24 10:18:47 +0900

    jsDoc

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ document.createElement
 
 
 //자바스크립트로 되어있는 API 모듈 등을 갖어올때는 정의파일(타입정의해놓은)이 존재해야한다 .
-import {init,exit} from "./myPack";
+
+//import {init,exit} from "myPack"; // 정의파일을 통해 myPack.js파일의 함수를 사용 
+import {init,exit} from "./myPack";   // myPack.js 파일을 갖어와 타입스크립트가 추론해서 사용
 init({url:'df'})
 exit(2)
--- src/myPack.js
+++ src/myPack.js
@@ -1,6 +1,23 @@
+
+//@ts-check
+
+
+// jsDoc  자바스크립트 코드를 바꾸지 않고 주석을 달아주는것만으로도 타입스크립트가 알아서 해석 /**   */ 
+/**
+ * 
+ * @param {object} config 
+ * @param {string}config.url
+ * @returns {boolean}
+ */
 export function init(config){
     return true
 }
+
+/**
+ * 
+ * @param {number} num 
+ * @returns number
+ */
 export function exit( num){
     return num + 1
 }//

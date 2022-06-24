dfae69dcf819f7b5aed4121a7b0cb5a86dc4bd99
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-06-24 14:46:15 +0900

    blockchain

--- build/index.js
+++ build/index.js
@@ -1,3 +1,32 @@
-"use strict";
 function f() { return 'hello'; }
 console.log(f());
+import * as crypto from "crypto";
+class block {
+    constructor(prevhash, height, data) { this.hash = block.cal(prevhash, height, data); }
+    static cal(prevhash, height, data) {
+        const str = `${prevhash}${height}${data}`;
+        return crypto.createHash('sha256').update(str).digest("hex");
+    }
+}
+class blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    getprevhash() {
+        if (this.blocks.length === 0) {
+            return "";
+        }
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addblock(data) {
+        const termblock = new block(this.getprevhash(), this.blocks.length, data);
+        this.blocks.push(termblock);
+    }
+    getchain() {
+        return [...this.blocks];
+    }
+}
+const newChain = new blockchain;
+newChain.addblock('first');
+newChain.addblock('second');
+console.log(newChain.getchain);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
 function f(){return 'hello'}
 console.log(f())
+
+import * as crypto from "crypto"
+
+interface block{
+    hash:string
+    prevhash:string
+    height:number
+    data:string
+}
+class block implements block{
+    constructor(
+        prevhash:string,
+        height:number,
+        data:string
+    ){this.hash=block.cal(prevhash,height,data)}
+    static cal(prevhash:string,height:number,data:string){
+        const str=`${prevhash}${height}${data}`
+        return crypto.createHash('sha256').update(str).digest("hex")
+    }
+}
+
+
+class blockchain {
+    private blocks:block[]
+    constructor(){
+        this.blocks=[]
+    }
+    getprevhash(){
+        if(this.blocks.length === 0){return ""}
+        return this.blocks[this.blocks.length-1].hash
+    }
+    addblock(data:string){
+        const termblock = new block(this.getprevhash(),this.blocks.length,data)
+        this.blocks.push(termblock)
+      }
+    getchain(){
+        return [...this.blocks]
+    }
+}
+
+const newChain = new blockchain
+newChain.addblock('first')
+newChain.addblock('second')
+console.log(newChain.getchain)
